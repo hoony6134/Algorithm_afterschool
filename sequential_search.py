80483e0a79e8290a5ac7ac34b12c21b3ff411299
JeongHoon Lim <hoony6134@gmail.com>
2022-10-24 17:30:11 +0900

    File name edited and added .gitignore

--- sequential_search.py
+++ sequential_search.py
@@ -2,6 +2,6 @@ list=[2,6,9,50,73,102,999]
 def sequential_search(list, item):
     for i in range(len(list)):
         if list[i] == item:
-            print("Found at index",i)
+            print("Found at position",i+1)
     return 'Not in list'
 print(sequential_search(list, int(input("Enter a number: "))))
